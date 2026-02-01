7a137cf8e5c48223ecfeedb85dd8dd18a2974e4c
difurka <e.balakina@g.nsu.ru>
2026-02-01 21:08:29 +0700

    start decoder

--- lesson2/decoder.c
+++ lesson2/decoder.c
@@ -8,23 +8,35 @@
 static char  parse_reg(const enum reg_t reg) {
     switch (reg)
     {
-    case 'A': return 'A';
-    case 'B': return 'B';
-    case 'C': return 'C';
-    case 'D': return 'D';
+    case A: return 'A';
+    case B: return 'B';
+    case C: return 'C';
+    case D: return 'D';
     default:
         fprintf(stderr, "unknown symbol<%c>", reg);
     }
 }
 
+char* in_bin(int dec) {
+    int num_elements = 8;
+    int num_bits = sizeof(int) * num_elements;
+    char* res = malloc(num_bits + 1);
+    
+    for (int i = 8; i >= 0; i--) {
+        res[8 - 1 - i] = ((dec >> i) & 1) + '0';
+    }
+    res[8] = '\0';
+    return res;
+}
 
 int main() {
     char mnemonic[16] = {0};
-
-    while (scanf("%15s", mnemonic) == 1) {
-        if (strlen(mnemonic) == 4 || mnemonic[0] == '0' || mnemonic[1]=='x')
-        printf("%15s\n", substr(mnemonic,2));
+    int value;
+    while (scanf("%X", &value) == 1) {
+        // if (strlen(mnemonic) == 4 || mnemonic[0] == '0' || mnemonic[1]=='x')
+        printf("%s\n", in_bin(value));
         // fflush(stdout);
+        // strncpy(char* destination, const char* source, size_t num)
     }
     printf("\n");
 }
